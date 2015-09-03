d4def6a8e06d1318fa673ab49c7c8e2d85d709c7
vinay427 <vhiremath95@gmail.com>
2015-09-03 11:08:19 -0400

    Added CORS header (again?)

--- newsletter.js
+++ newsletter.js
@@ -1,38 +1,40 @@
 var request = require('koa-request');
 
 var newsletter = function* (next) {
-	this.body = this.request.body;
-        var name = this.body.name.split(' ');
+    this.body = this.request.body;
+    this.set('Access-Control-Allow-Origin', '*');
 
-        var data = {
-                email_address: this.body.email,
-                status: 'subscribed',
-                merge_fields: {
-                        FNAME: name[0],
-                        LNAME: name[1]
-                }
-        };
+    var name = this.body.name.split(' ');
 
-        var url = 'https://user:' + process.env.OPTIMIZE_MAILCHIMP_API_KEY
-                + '@us6.api.mailchimp.com/3.0/lists/'
-                + process.env.OPTIMIZE_MAILCHIMP_LIST_ID + '/members';
+    var data = {
+	email_address: this.body.email,
+	status: 'subscribed',
+	merge_fields: {
+	    FNAME: name[0],
+	    LNAME: name[1]
+	}
+    };
 
-        var options = {
-                url: url,
-                body: data,
-                json: true
-        }
+    var url = 'https://user:' + process.env.OPTIMIZE_MAILCHIMP_API_KEY
+	    + '@us6.api.mailchimp.com/3.0/lists/'
+	    + process.env.OPTIMIZE_MAILCHIMP_LIST_ID + '/members';
 
-        var response = yield request.post(options);
-        if (response.statusCode === 200) {
-                this.body = {status: 'success',
-			    data: data};
-        }
-        else {
-                this.status = response.statusCode;
-                this.body = {status: 'error',
-			     message: response.body.detail};
-        }	
+    var options = {
+	url: url,
+	body: data,
+	json: true
+    }
+
+    var response = yield request.post(options);
+    if (response.statusCode === 200) {
+	this.body = {status: 'success',
+		     data: data};
+    }
+    else {
+	this.status = response.statusCode;
+	this.body = {status: 'error',
+		     message: response.body.detail};
+    }
 }
 
 module.exports = newsletter;
